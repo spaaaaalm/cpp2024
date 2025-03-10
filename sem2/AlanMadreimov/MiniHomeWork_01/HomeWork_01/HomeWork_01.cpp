8872ef7e36026a3e7559caf1c1417ecdde7d1c31
AlanMadreimov <madreimovalan@gmail.com>
2025-03-11 01:30:11 +0300

    Sem 2 HomeWork_01
    Lalala

---
+++ sem2/AlanMadreimov/MiniHomeWork_01/HomeWork_01/HomeWork_01.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <HW_01.h>
+
+int main()
+{
+    std::vector <float> arr = {8,6,0,10};
+    std::cout << srznach(arr) << "\n";
+    std::cout << med(arr) << "\n";
+    std::cout << skvad(arr) << "\n";
+    std::cout << dis(arr) << "\n";
+}
+
