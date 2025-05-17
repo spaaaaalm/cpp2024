98f52d5df4cf8c137dab3a9b4c7c469b0c5dfc26
RomanLitvinchuk <roman67877@gmail.com>
2025-05-17 17:04:04 +0300

    Add MiniHomework4

---
+++ sem1/RomanLitvinchuk/sem2/MiniHomework4/MiniHomework4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int main() {
+    std::vector<std::string> strings = { "behave", "word", "anything", "example" };
+
+    auto getLengths = [](const std::vector<std::string>& vec) {
+        std::vector<size_t> lengths;
+        for (const auto& str : vec) {
+            lengths.push_back(str.size());
+        }
+        return lengths;
+        };
+    std::vector<size_t> lengths = getLengths(strings);
+    for (auto leng : lengths) 
+    {
+        std::cout << leng << std::endl;
+    }
+
+    return 0;
+}
