dfaaf1dbc27e24b69bf64e11ffa000e4be033dd9
Dentera <artem.loskutov@gmail.com>
2025-05-20 17:11:36 +0300

    Minihomework_4_sem2

---
+++ sem2/ArtyomLoskutov/Minihomework_4/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+    std::vector<std::string> book{ "123", "abc" };
+
+    auto len = [&book]()
+        {
+            for (std::string paper : book)
+            {
+                std::cout << paper.length();
+            }
+        };
+}
