1f95710b3e89a9f6affe324ee0635879d77a868e
Borodai Vladimir <vovan_18@icloud.com>
2025-06-06 11:05:24 +0300

    MiniHWs(BorodaiVladimir)

---
+++ sem2/BorodaiVladimir/MiniHW4/miniHW4.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    std::vector<std::string> strings = { "SumoTatami", "c++", "HelloWorld", "LambdaFunction" };
+
+    for (const auto& str : strings) {
+        auto length = [](const std::string& s) { return s.length(); }(str);
+        std::cout << length << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
